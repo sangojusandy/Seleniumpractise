dc2bb9c356689ee3be4dd86d2bb51fbba6e2f16c
QUALITEST\sandeep.sangoju <sandeep.sangoju@keyloop.com>
2023-08-24 00:19:52 +0530

    first commit

---
+++ src/main/java/helperclasses/WebelementHelpers.java
@@ -0,0 +1,17 @@
+package helperclasses;
+
+public class WebelementHelpers {
+    public void click(){
+        System.out.println("click");
+    }
+    public void sendKeys(){
+        System.out.println("sendKeys");
+    }
+    public void scrollUp(){
+        System.out.println("scrollUp");
+    }
+    public void scrollDown(){
+        System.out.println("scrollDown");
+    }
+
+}
---
+++ src/test/java/stepdefinitions/login_stepdef.java
@@ -0,0 +1,4 @@
+package stepdefinitions;
+
+public class login_stepdef {
+}
