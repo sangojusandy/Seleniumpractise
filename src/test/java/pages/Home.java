8308da249f49f499622f8e7ce026a6cff22753f0
QUALITEST\sandeep.sangoju <sandeep.sangoju@keyloop.com>
2023-08-25 11:48:09 +0530

    todaycommit

--- src/test/java/pages/Home.java
+++ src/test/java/pages/Home.java
@@ -1,4 +1,35 @@
 package pages;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Home {
+    public static void main(String[] args) {
+        Map<Integer,Integer> m=new HashMap<>();
+        m.put(1, 2);
+        m.put(2, 3);
+        m.put(3, 4);
+        //System.out.print(m.get(1));
+//		if(m.containsValue("Pen")) {
+//			System.out.println("true");
+//		}
+
+
+
+int highest=0;
+
+        for(Map.Entry<Integer,Integer> f : m.entrySet())
+        {
+
+           int x=f.getValue();
+           if(x>highest){
+               highest=x;
+           }
+
+
+
+
+        }
+
+    }
 }
